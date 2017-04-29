f34ac94430cacace9eac86cd4f46fd8ec3ed782a
Xinyu Xie <xinyux@g.ucla.edu>
2017-04-29 14:49:40 -0700

    example.pl

---
+++ Prolog/example.pl
@@ -0,0 +1,14 @@
+worksln(bill, sales).
+worksln(sally, accounts).
+
+deptManager(sales, joan).
+deptManager(accounts, henry).
+
+managerOf(joan, james).
+managerOf(henry, james).
+managerOf(james, paul).
+managerOf(W, M) :- worksln(W, Dept), deptManager(Dept, M).
+
+superiorOf(E, S) :- managerOf(E, S).
+superiorOf(E, S) :- managerOf(E, M), superiorOf(M, S).
+
