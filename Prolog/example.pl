6013cfe9278cd7366e78cd614e8890eb64ad6be9
Xinyu Xie <xinyux@g.ucla.edu>
2017-04-29 14:55:56 -0700

    add query example

--- Prolog/example.pl
+++ Prolog/example.pl
@@ -12,3 +12,10 @@ managerOf(W, M) :- worksln(W, Dept), deptManager(Dept, M).
 superiorOf(E, S) :- managerOf(E, S).
 superiorOf(E, S) :- managerOf(E, M), superiorOf(M, S).
 
+/** <query examples>
+
+?- superiorOf(X, Y).
+?- aggregate_all(count, (superiorOf(X, Y)), Count).
+?- managerOf(X, Y).
+
+*/
