1b9adff30a5967802a76b15ec56ed432f1a0fd7e
Xinyu Xie <xinyux@g.ucla.edu>
2017-05-11 18:48:04 -0700

    add model.py

---
+++ Project/feature_eng.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import timedelta
+
+
+def process_csv(read_file, interval, file_name):
+    print ("reading file: {0} ".format(read_file))
+    csv_file = pd.read_csv(read_file, low_memory=False)
+    csv_file.columns = ["epoch_time", "x", "y", "z", "num_label", "string_label"]
+    csv_file['date_time'] = pd.to_datetime(csv_file['epoch_time'], unit='ms') + timedelta(hours=-7)
+    csv_file.index = csv_file['date_time']
+    csv_file['x'] = np.float64(csv_file['x'])
+    csv_file['y'] = np.float64(csv_file['y'])
+    csv_file['z'] = np.float64(csv_file['z'])
+    csv_file['m'] = np.sqrt(csv_file['x']**2 + csv_file['y']**2 + csv_file['z']**2)
+
+    csv_file = csv_file[['x','y','z','m']]
+
+    # Select index range
+    # csv_file = csv_file.loc['2017-05-07 02:30:00':'2017-05-07 04:55:00']
+
+    # Downsampling
+    avg_file = csv_file.resample(interval, label='left').mean()
+    min_file = csv_file.resample(interval, label='left').min()
+    max_file = csv_file.resample(interval, label='left').max()
+    std_file = csv_file.resample(interval, label='left').std()
+    avg_file.columns = [file_name + "_x_avg", file_name + "_y_avg", file_name + "_z_avg", file_name + "_m_avg"]
+    min_file.columns = [file_name + "_x_min", file_name + "_y_min", file_name + "_z_min", file_name + "_m_min"]
+    max_file.columns = [file_name + "_x_max", file_name + "_y_max", file_name + "_z_max", file_name + "_m_max"]
+    std_file.columns = [file_name + "_x_std", file_name + "_y_std", file_name + "_z_std", file_name + "_m_std"]
+
+    # merge files
+    csv_file = pd.merge(avg_file, min_file, left_index=True, right_index=True)
+    csv_file = pd.merge(csv_file, max_file, left_index=True, right_index=True)
+    csv_file = pd.merge(csv_file, std_file, left_index=True, right_index=True)
+
+    # Print data collection period
+    # print (csv_file.head(1).index)
+    # print (csv_file.tail(1).index)
+    return csv_file
+
+
+def feature_eng(folder_names, file_names, interval, sleep_label):
+    print ("Start feature engineering")
+    for folder in folder_names:
+        print ("Processing folder {0}".format(folder))
+        csv_file = pd.DataFrame()
+        for file_name in file_names:
+            read_file = "./Data/{0}/{1}.data.csv".format(folder, file_name)
+            if csv_file.empty:
+                csv_file = process_csv(read_file, interval, file_name)
+            else:
+                csv_file = pd.merge(csv_file, process_csv(read_file, interval, file_name), left_index=True, right_index=True)
+        label_csv(csv_file, sleep_label[folder])
+        csv_file.to_csv("./LabeledData/{0}.csv".format(folder))
+
+
+def label_csv(csv_file, sleep_interval):
+    csv_file['sleep'] = '0'
+    csv_file.loc[sleep_interval[0]:sleep_interval[1], 'sleep'] = '1'
+
+
+def plot_csv(csv_file, column):
+    print ("plot file: {0}".format(csv_file))
+    df = pd.DataFrame(csv_file, index=csv_file.index, columns=column)
+    df.plot()
+    plt.legend()
+
+
+def main():
+    interval = '60S'
+    folder_names = ["May_09_2017","May_10_2017","May_11_2017"]
+    file_names = ["1_android.sensor.accelerometer", "2_android.sensor.magnetic_field", "3_android.sensor.orientation",
+                  "4_android.sensor.gyroscope", "9_android.sensor.gravity", "10_android.sensor.linear_acceleration"]
+    sleep_label = {"May_09_2017": ['2017-05-09 01:45:00','2017-05-09 09:42:00'],
+                   "May_10_2017": ['2017-05-10 00:24:00','2017-05-10 07:05:00'],
+                   "May_11_2017": ['2017-05-11 01:15:00','2017-05-11 10:10:00']}
+    feature_eng(folder_names, file_names, interval, sleep_label)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Project/model.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import glob
+import sklearn as sk
+import sklearn.ensemble as en
+import sklearn.tree as tree
+import sklearn.linear_model as linear
+import sklearn.naive_bayes as nb
+import sklearn.neural_network as nn
+import sklearn.feature_selection as fs
+import sklearn.model_selection as ms
+
+
+def random_forest_classifier(train_data,features,n_trees=50,cv_folds=5):
+    model = en.RandomForestClassifier(n_estimators=n_trees)
+    print "Testing Random Forest model..."
+    scores = ms.cross_val_score(model, train_data[features], train_data["sleep"], cv=cv_folds)
+    print "Random Forest. Average Score over %d folds is %0.2f (+/- %0.2f)" % (cv_folds,scores.mean(),scores.std() * 2)
+    model.fit(train_data[features],train_data["sleep"])
+    return {"model":model,"score":scores.mean()}
+
+
+def decision_tree_classifier(train_data,features,cv_folds=5):
+    model = tree.DecisionTreeClassifier()
+    print "Testing Decision Tree model..."
+    scores = ms.cross_val_score(model, train_data[features], train_data["sleep"], cv=cv_folds)
+    print "Decision Tree. Average Score over %d folds is %0.2f (+/- %0.2f)" % (cv_folds,scores.mean(),scores.std() * 2)
+    model.fit(train_data[features],train_data["sleep"])
+    return {"model":model,"score":scores.mean()}
+
+
+def adaboost_classifier(train_data,features,n_estimators=50,cv_folds=5):
+    model = en.AdaBoostClassifier(n_estimators=n_estimators)
+    print "Testing Adaboost model..."
+    scores = ms.cross_val_score(model, train_data[features], train_data["sleep"], cv=cv_folds)
+    print "Adaboost. Average Score over %d folds is %0.2f (+/- %0.2f)" % (cv_folds,scores.mean(),scores.std() * 2)
+    model.fit(train_data[features],train_data["sleep"])
+    return {"model":model,"score":scores.mean()}
+
+
+def gradient_boosting_classifier(train_data,features,cv_folds=5):
+    model = en.GradientBoostingClassifier(loss="exponential")
+    print "Testing Gradient Boosting model..."
+    scores = ms.cross_val_score(model, train_data[features], train_data["sleep"], cv=cv_folds)
+    print "Gradient Boosting. Average Score over %d folds is %0.2f (+/- %0.2f)" % (cv_folds,scores.mean(),scores.std() * 2)
+    model.fit(train_data[features],train_data["sleep"])
+    return {"model":model,"score":scores.mean()}
+
+
+def logistic_classifier(train_data,features,cv_folds=5):
+    model = linear.LogisticRegression()
+    print "Testing Logistic Regression model..."
+    scores = ms.cross_val_score(model, train_data[features], train_data["sleep"], cv=cv_folds)
+    print "Logistic. Average Score over %d folds is %0.2f (+/- %0.2f)" % (cv_folds,scores.mean(),scores.std() * 2)
+    model.fit(train_data[features],train_data["sleep"])
+    return {"model":model,"score":scores.mean()}
+
+
+def bayes_classifier(train_data,features,cv_folds=5):
+    model = nb.BernoulliNB()
+    print "Testing Naive Bayes model..."
+    scores = ms.cross_val_score(model, train_data[features], train_data["sleep"], cv=cv_folds)
+    print "Naive Bayes. Average Score over %d folds is %0.2f (+/- %0.2f)" % (cv_folds,scores.mean(),scores.std() * 2)
+    model.fit(train_data[features],train_data["sleep"])
+    return {"model":model,"score":scores.mean()}
+
+
+def neural_network_classifier(train_data,features,cv_folds=5):
+    model = sk.neural_network.MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(25, 15, 3), random_state=1)
+    scores = ms.cross_val_score(model, train_data[features], train_data["sleep"], cv=cv_folds)
+    print "Neural Network. Average Score over %d folds is %0.2f (+/- %0.2f)" % (cv_folds,scores.mean(),scores.std() * 2)
+    model.fit(train_data[features],train_data["sleep"])
+    return {"model":model,"score":scores.mean()}
+
+
+def main():
+    # Generate train data
+    all_files = glob.glob("./LabeledData/May_*.csv")
+    train_data = pd.DataFrame()
+    list_ = []
+    for file_ in all_files:
+        print ("Reading file {0} into train_data".format(file_))
+        df = pd.read_csv(file_, index_col=None, header=0)
+        list_.append(df)
+    train_data = pd.concat(list_)
+
+    # Generate feature sets
+    features = []
+    # sensors = ["1_android.sensor.accelerometer", "3_android.sensor.orientation",
+    #            "4_android.sensor.gyroscope", "9_android.sensor.gravity",
+    #            "10_android.sensor.linear_acceleration"]
+    sensors = ["4_android.sensor.gyroscope"]
+    variables = ["_x", "_y", "_z", "_m"]
+    functions = ["_avg", "_min", "_max", "_std"]
+    for sensor in sensors:
+        for variable in variables:
+            for function in functions:
+                features.append(sensor+variable+function)
+
+    # Modeling
+    results = decision_tree_classifier(train_data, features)
+    print (results["score"])
+    results = random_forest_classifier(train_data, features)
+    print (results["score"])
+    results = adaboost_classifier(train_data, features)
+    print (results["score"])
+    results = gradient_boosting_classifier(train_data, features)
+    print (results["score"])
+    results = bayes_classifier(train_data, features)
+    print (results["score"])
+    results = logistic_classifier(train_data, features)
+    print (results["score"])
+    results = neural_network_classifier(train_data, features)
+    print (results["score"])
+
+
+if __name__ == '__main__':
+    main()
